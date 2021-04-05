0a81f996ce7a87855b888acfa080c7f05bed7063
teamkitkumazaki <41033533+teamkitkumazaki@users.noreply.github.com>
2021-04-05 09:19:29 +0900

    カートページのレスポンシブレイアウトを構築

--- js/admin.js
+++ js/admin.js
@@ -5,6 +5,39 @@ $(function() {
     var optionItemHeight = [];
     var optionItemLabel = [];
     var optionItemWrap = [];
+    var deliveryDate = $('#deliveryDate');
+    var today = new Date();
+    var y;
+    var m;
+    var paramM;
+    var d;
+    var paramD;
+    var w;
+    var paramW;
+    console.log('getDay:' + w);
+    var wd = ['日', '月', '火', '水', '木', '金', '土'];
+
+    function getDayDisplay(day){
+      var date = new Date();
+      date.setDate(date.getDate() + day);
+      y  = date.getFullYear();
+      m = date.getMonth() + 1;
+      d = date.getDate();
+      w = date.getDay() % 7;
+      console.log(m + '月' + d + '日' + '(' + wd[w] + ')');
+      if(m < 10){ paramM = '0' + m }else{ paramM = m }
+      if(d < 10){ paramD = '0' + d }else{ paramD = d }
+      var dayParam = y + '/' + paramM + '/' + paramD;
+      var dayDisplay = m + '月' + d + '日' + '(' + wd[w] + ')';
+      deliveryDate.append('<option value="' + dayParam + '">' + dayDisplay + '</option>');
+    }
+
+    function setDayDisplay(){
+      for (var i=0; i<14; i++) {
+        getDayDisplay(i);
+      }
+    }
+
     function init(){
       $.each(target.find('.option_item'), function(index) {
         optionItem[index] = $(this);
@@ -21,6 +54,7 @@ $(function() {
           }
         });
       });
+      setDayDisplay();
     }
 
     init();
