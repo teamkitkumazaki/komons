da2895cd7f1e9f4ff974660867c1d0d704eb13dd
teamkitkumazaki <41033533+teamkitkumazaki@users.noreply.github.com>
2021-05-07 16:40:40 +0900

    配送指定日出力スクリプトを実装

--- js/admin.js
+++ js/admin.js
@@ -43,7 +43,6 @@ $(function() {
       });
     });
 
-    console.log('optionState:' + optionState);
     if(optionState.indexOf('熨斗') !=  -1){
       $('#noshi1').css({'display': 'block'});
       $('#noshi2').css({'display': 'block'});
@@ -70,130 +69,165 @@ if (document.getElementById('cart')) {
   controllProductQuantity($('#cartItem'));
 }
 
-  function cartOptionController(target){
-    var deliveryDateList = $('#deliveryDateList');
-    var deliveryDateListTxt = deliveryDateList.text().length;
-    console.log('length:' + deliveryDateListTxt);
-    var optionItem = [];
-    var optionItemHeight = [];
-    var optionItemLabel = [];
-    var optionItemWrap = [];
-    var finalPrice = $('#finalPrice').html();
-    var priceCulc = finalPrice.replace(/,/g, '');
-    var wd = ['日', '月', '火', '水', '木', '金', '土'];
-    var setDayStatus = 0;
-    var deliveryDate = $('#deliveryDate');
-    var deliveryTime = $('#deliveryTime');
-    var messageCard = $('textarea[name="messeage_card"]');
-    var memoWrap = $('textarea[name="memo_wrap"]');
-    var noshiType = $('input[name="noshi_type"]');
-    var noshiName = $('input[name="noshi_name"]');
-    var setValue;
+function cartOptionController(target){
+  var deliveryDateList = $('#deliveryDateList');
+  var optionItem = [];
+  var optionItemHeight = [];
+  var optionItemLabel = [];
+  var optionItemWrap = [];
+  var finalPrice = $('#finalPrice').html();
+  var priceCulc = finalPrice.replace(/,/g, '');
+  var delay = 0; /*曜日・時間による配送日のスライド設定*/
+  var today = new Date();
+  var startDay;
+  var y;
+  var m;
+  var paramM;
+  var d;
+  var paramD;
+  var w;
+  var paramW;
+  var time;
+  var num;
+  var testFlg = Number(0);
+  var wd = ['日', '月', '火', '水', '木', '金', '土'];
+  var holiday = [/*2021年度*/ '7/22', '7/23','8/9','9/20','9/23', '11/3', '11/23','12/29', '12/30', '12/31',/*2022年度*/ '1/1', '1/2', '1/3', '1/10', '2/11', '2/23', '3/21', '4/29', '5/3', '5/4', '5/5'];
+  var setDayStatus = 0;
+  var deliveryDate = $('#deliveryDate');
+  var deliveryTime = $('#deliveryTime');
+  var messageCard = $('textarea[name="messeage_card"]');
+  function setMessageCardVal(){
+    var messageTxt = messageCard.val();
+    $('#messageCartContent').attr('value', messageTxt);
+  }
 
-    function setMessageCardVal(){
-      var messageTxt = messageCard.val();
-      console.log(messageTxt);
-      $('#messageCartContent').attr('value', messageTxt);
+  function getDayDisplay(day){
+    var date = new Date();
+    date.setDate(date.getDate() + day + testFlg);
+    y  = date.getFullYear();
+    m = date.getMonth() + 1;
+    d = date.getDate();
+    w = date.getDay() % 7;
+    time = date.getHours();
+    if(m < 10){ paramM = '0' + m }else{ paramM = m }
+    if(d < 10){ paramD = '0' + d }else{ paramD = d }
+    var dayParam = y + '-' + paramM + '-' + paramD;
+    var dayDisplay = m + '月' + d + '日' + '(' + wd[w] + ')';
+    deliveryDate.append('<option value="' + dayParam + '">' + dayDisplay + '</option>');
+  }
+
+  function setDayDisplay(num){
+    var startSet = Number(num) + Number(2);
+    console.log('startSet:' + startSet);
+    for (var i= startSet; i<30; i++) {
+      getDayDisplay(i);
     }
+  }
 
-    function setDayDisplay(){
-      if(setDayStatus == 0){
-        $.each(deliveryDateList.find('.calendar_dateWrap'), function(index) {
-            $(this).find('div').addClass('date_item');
-            $(this).find('button').addClass('date_item');
-            $(this).find('.date_item').each(function(index) {
-              var weakSetNumber = index % 7;
-              $(this).attr('week', weakSetNumber);
-            });
-            $(this).find('.is-active').each(function(index) {
-              var dateTxt = $(this).attr('data-date');
-              var res = dateTxt.split('/');
-              var dateWeak = $(this).attr('week');
-              var dayTxtSet = res[1] + '月' +  res[2] + '日(' + wd[dateWeak] + ')';
-              if(res[1] < 10){
-                res[1] = '0' + res[1]
-              }
-              if(res[2] < 10){
-                res[2] = '0' + res[2]
-              }
-              var dayTxtValue = res[0] + '-' +  res[1] + '-' + res[2];
-              deliveryDate.append('<option value="' + dayTxtValue + '">' + dayTxtSet + '</option>');
-            });
-            setDayStatus = 1;
-          });
-      };
-    }
-
-    function init(){
-      console.log('priceCulc:' + priceCulc);
-      if(priceCulc > 5499){
-        console.log('pattern1')
-        $('#shippingPrice').html('送料無料');
-      }else if(optionState.indexOf('定期') == 1){
-        console.log('pattern3')
-        $('#shippingPrice').html('送料無料');
+  function setShippingDay2(delay){
+    console.log('delay:' + delay);
+    today.setDate(today.getDate() + delay);
+    var todayY = today.getFullYear();
+    var todayM = today.getMonth() + 1;
+    var todayD = today.getDate();
+    var todayW = today.getDay() % 7;
+    var dayTime = today.getHours();
+    var todayDate = todayM + '/' + todayD;
+    if(todayDate){
+      console.log($.inArray(todayDate,holiday));
+      console.log('todayDate:' + todayDate);
+      if($.inArray(todayDate,holiday) != -1){
+        console.log('holiday');
+        delay = delay + 1;
+        setShippingDay2(delay);
       }else{
-        console.log('pattern2')
-        var shippingTerm = Number(5500) - Number(priceCulc);
-        $('#shippingPrice').html('<span class="amount_price">660円</span>(税込)');
-        $('#shippingFree').html('※あと' + shippingTerm.toLocaleString() + '円で送料無料')
+        console.log('OK');
+        console.log('delay:' + delay);
+        setDayDisplay(delay);
       }
-      deliveryTime.on({
-        'change': function() {
-          var timeValue =  $(this).val();
-          $('#delivery-time').val(timeValue);
-        }
-      });
-      messageCard.on({
-        'keyup': function() {
-          setMessageCardVal();
-        }
-      });
-      deliveryDate.on({
-        'click': function() {
-          setDayDisplay();
-        }
-      });
-
-      setInterval(function(){
-        var deliveryDateListTxtNew = deliveryDateList.text().length;
-        if(deliveryDateListTxt != deliveryDateListTxtNew && setDayStatus == 0){
-          setDayDisplay();
-        }
-      },1000);
-
-
-
-      $(window).on({
-        'scroll': function() {
-          setDayDisplay();
-        }
-      });
-
-      $.each(target.find('.option_item'), function(index) {
-        optionItem[index] = $(this);
-        optionItemHeight[index] = $(this).outerHeight();
-        optionItemLabel[index] = $(this).find('label');
-        optionItemWrap[index] = $(this).find('.input_wrap').outerHeight();
-        optionItemLabel[index].on({
-          'click': function() {
-            if($(this).find('input[type=checkbox]:checked').val() == 'true'){
-              optionItem[index].css({height: optionItemHeight[index] + optionItemWrap[index] + 15 + 'px'});
-            }else{
-              optionItem[index].css({height: optionItemHeight[index] + 'px'});
-            }
-          }
-        });
-      });
     }
-
-    init();
-
   }
 
-  if (document.getElementById('cart')) {
-    cartOptionController($('article'));
+  function setShippingDay(){
+    today.setDate(today.getDate() + testFlg);
+    console.log(today);
+    var dayTime = today.getHours();
+    var todayW = today.getDay() % 7;
+    console.log('todayW:' + wd[todayW]);
+    if(dayTime < 10){ /* 10時までの注文の場合 */
+      if(todayW == 0){ /* 日曜日の場合 */
+        setShippingDay2(1) /* 配送を月曜日にする*/
+      }else if(todayW == 6){ /* 土曜日の場合*/
+        setShippingDay2(2); /* 配送を月曜日にする*/
+      }else{ /*月~金曜日の注文の場合*/
+        setShippingDay2(0); /*当日に配送する*/
+      }
+    }else{ /* 10時以降の注文の場合 */
+      if(todayW == 6){ /* 土曜日の場合*/
+        setShippingDay2(2);/* 配送を月曜日にする*/
+      }else if(todayW == 5){ /* 金曜日の場合*/
+        setShippingDay2(3);/* 配送を月曜日にする*/
+      }else{ /*月~木曜日の注文の場合*/
+        setShippingDay2(1);/*翌日に配送する*/
+      }
+    }
   }
+
+  function setMessageCardVal(){
+    var messageTxt = messageCard.val();
+    console.log(messageTxt);
+    $('#messageCartContent').attr('value', messageTxt);
+  }
+
+  function init(){
+    if(priceCulc > 5499){
+      $('#shippingPrice').html('送料無料');
+    }else if(optionState.indexOf('定期') == 1){
+      $('#shippingPrice').html('送料無料');
+    }else{
+      var shippingTerm = Number(5500) - Number(priceCulc);
+      $('#shippingPrice').html('<span class="amount_price">660円</span>(税込)');
+      $('#shippingFree').html('※あと' + shippingTerm.toLocaleString() + '円で送料無料')
+    }
+    deliveryTime.on({
+      'change': function() {
+        var timeValue =  $(this).val();
+        $('#delivery-time').val(timeValue);
+      }
+    });
+    messageCard.on({
+      'keyup': function() {
+        setMessageCardVal();
+      }
+    });
+
+    setShippingDay();
+
+
+    $.each(target.find('.option_item'), function(index) {
+      optionItem[index] = $(this);
+      optionItemHeight[index] = $(this).outerHeight();
+      optionItemLabel[index] = $(this).find('label');
+      optionItemWrap[index] = $(this).find('.input_wrap').outerHeight();
+      optionItemLabel[index].on({
+        'click': function() {
+          if($(this).find('input[type=checkbox]:checked').val() == 'true'){
+            optionItem[index].css({height: optionItemHeight[index] + optionItemWrap[index] + 15 + 'px'});
+          }else{
+            optionItem[index].css({height: optionItemHeight[index] + 'px'});
+          }
+        }
+      });
+    });
+  }
+
+  init();
+
+}
+
+if (document.getElementById('cart')) {
+  cartOptionController($('article'));
+}
 
   function setCategoryName(){
     var domain = 'https://' + location.host;
@@ -266,7 +300,6 @@ if (document.getElementById('cart')) {
 
   function setHistoryBackLink(){
     var ref = document.referrer;
-    console.log('ref:' + ref);
     if ( ref.match(/collections/)) {
 
     }else if(ref.match(/products/)){
