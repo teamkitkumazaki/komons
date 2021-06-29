35d755c0fd1841af90eaf634438418cb540f934a
teamkitkumazaki <41033533+teamkitkumazaki@users.noreply.github.com>
2021-06-29 13:17:45 +0900

    フッターメルマガの修正

--- js/checkout.js
+++ js/checkout.js
@@ -5,13 +5,13 @@
 .sns_wrap ul{
   display: flex;
   flex-wrap: wrap;
-  justify-content: flex-start;
+  justify-content: space-between;
   align-items: center;
   margin-top: 10px;
 }
 .sns_wrap ul li{
-  width: 30px;
-  margin-right: 7px;
+  width: 49%;
+  margin-bottom: 10px;
 }
 .sns_wrap ul li:nth-last-of-type(1){
   margin-right: 0px;
@@ -20,15 +20,63 @@
   transition: opacity .3s;
   display: block;
   width: 100%;
-  height: 30px;
-  border-radius: 50%;
-  border: solid .6px #00381f;
+  padding: 10px;
+  box-sizing: border-box;
+  border-radius: 5px;
+  color: #fff;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  transition: hover .2s;
+}
+.sns_wrap ul li a:hover{
+  opacity: 0.85;
+}
+.sns_wrap ul li a .icon{
+  display: block;
+  height: 18px;
+  background-size: contain;
   background-repeat: no-repeat;
   background-position: center center;
-  background-size: 14px;
+  filter: brightness(100);
+}
+.sns_wrap ul li a .text{
+  display: block;
+  font-size: 14px;
+  line-height: normal;
+  letter-spacing: 0.5px;
+  margin-left: 10px;
+}
+.sns_wrap ul li a.tw{
+  background-color: #1da1f2;
+}
+.sns_wrap ul li a.tw .icon{
+  width: 17px;
+}
+.sns_wrap ul li a.fb{
+  background-color: #195eb8;
+}
+.sns_wrap ul li a.fb .icon{
+  width: 14px;
+}
+.sns_wrap ul li a.ig{
+   background-image: linear-gradient(100deg, rgba(193, 53, 132, 0.99), #f77737 100%);
+}
+.sns_wrap ul li a.ig .icon{
+  width: 15px;
 }
 .sns_wrap ul li a.line{
-  background-size: 24px;
+  background-color: #00c300;
+}
+.sns_wrap ul li a.line .icon{
+  width: 18px;
+}
+
+@media screen and (max-width: 720px){
+  .sns_wrap ul li{
+    width: 100%;
+    margin-bottom: 7px;
+  }
 }
 </style>
 <script type="text/javascript" src="https://cdn.shopify.com/s/files/1/0536/9544/7234/t/2/assets/jquery-3.3.1.min.js" charset="UTF-8"></script>
@@ -108,7 +156,7 @@ $(function() {
           addCartNoteDisplay();
         }
         $('.step__sections').append('<div class="content-box"><div class="content-box__row content-box__row--no-border"><h2>マイページ・会員登録</h2></div><!-- content-box --><div class="content-box__row"><div class="section__content"><p style="display:block; margin-bottom: 5px;">▼注文内容・履歴の確認はこちらから</p><a href="/account" style="display: block; margin-bottom: 15px;">マイページ(アカウントをお持ちの方)</a><span class="register_ttl" style="display:block; margin-bottom: 5px;">▼アカウントをお持ちでない方は</span><a href="/account/register" style="display: block;">アカウントの作成</a></div><!-- section__content --></div><!-- content-box__row --></div><!-- content-box -->');
-        $('.step__sections').append('<div class="content-box"><div class="content-box__row content-box__row--no-border"><h2>公式SNSアカウント</h2><div class="os-step__special-description"></div></div><!-- content-box --><div class="content-box__row"><p class="os-step__description">商品についての情報や、新商品の販売などの最新情報をお届けしています。</p><div class="section__content"><div class="sns_wrap"><ul><li><a class="tw" href="https://business.facebook.com/komons.japan/" target="_blank" style="background-image: url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/tw_icon_8bb48353-f5e2-4860-a354-16f715f99236.png?v=1623519333)"></a></li><li><a class="fb" href="https://business.facebook.com/komons.japan/" target="_blank" style="background-image: url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/fb_icon_747b4124-db39-4841-baaa-1dca0b54e6e9.png?v=1623519333)"></a></li><li><a class="ig" href="https://www.instagram.com/komons.jp/" target="_blank" style="background-image: url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/ig_icon_f4cb92c6-0f94-4a4a-84be-4b348afd04e5.png?v=1623519333)"></a></li><li><a class="line" href="https://line.me/R/ti/p/%40266mlpqk" target="_blank" style="background-image: url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/line_icon_99a97e0c-93fa-4466-ba66-d8bfd6c524ec.png?v=1623519333)"></a></li></ul></div></div><!-- section__content --></div><!-- content-box__row --></div><!-- content-box -->');
+        $('.step__sections').append('<div class="content-box"><div class="content-box__row content-box__row--no-border"><h2>公式SNSアカウント</h2><div class="os-step__special-description"></div></div><!-- content-box --><div class="content-box__row"><p class="os-step__description">新商品の発売や、各種イベントなどに関する情報を発信しています。<br>ぜひフォローお願いいたします！</p><div class="section__content"><div class="sns_wrap"><ul><li><a class="tw" href="https://twitter.com/komons_jp" target="_blank"><span class="icon" style="background-image: url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/tw_icon_8bb48353-f5e2-4860-a354-16f715f99236.png?v=1623519333);"></span><span class="text">Twitter</span></a></li><li><a class="fb" href="https://business.facebook.com/komons.japan/" target="_blank"><span class="icon" style="background-image: url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/fb_icon_747b4124-db39-4841-baaa-1dca0b54e6e9.png?v=1623519333)"></span><span class="text">Facebook</span></a></li><li><a class="ig" href="https://www.instagram.com/komons.jp/" target="_blank"><span class="icon" style="background-image:url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/ig_icon_f4cb92c6-0f94-4a4a-84be-4b348afd04e5.png?v=1623519333)"></span><span class="text">Instagram</span></a></li><li><a class="line" href="https://line.me/R/ti/p/%40266mlpqk" target="_blank"><span class="icon" style="background-image: url(https://cdn.shopify.com/s/files/1/0536/9544/7234/files/line_icon_2.png?v=1624813774)"></span><span class="text">LINE</span></a></li></ul></div></div><!-- section__content --></div><!-- content-box__row --></div><!-- content-box -->');
 
         if(paymentMethod == '代金引換'){
           addCODPrice();
