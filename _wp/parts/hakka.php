277637d4c5d6540a05ae0e7e8b4e587f416c1608
teamkitkumazaki <41033533+teamkitkumazaki@users.noreply.github.com>
2022-04-15 13:06:30 +0900

    ハッカファブリックミストの販売開始

--- _wp/parts/hakka.php
+++ _wp/parts/hakka.php
@@ -2,90 +2,56 @@
 	<h3 class="journal_product_ttl">ITEM LIST</h3>
 	<ul class="item_wrapper">
 		<li class="item_box">
-			<div class="img_wrap"><a href="/products/k0066"><img src="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_04b87142-5bd6-4820-8734-8898892447fb.jpg?v=1649157015&amp;width=520" alt="2022 Summer &quot;北海道・和ハッカ&quot; / Free as a Bird" srcset="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_04b87142-5bd6-4820-8734-8898892447fb.jpg?v=1649157015&amp;width=352 352w" width="520" height="560"></a></div>
+			<div class="img_wrap"><a href="https://komons-japan.com/products/k0066"><img src="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_04b87142-5bd6-4820-8734-8898892447fb.jpg?v=1649157015&amp;width=520" alt="2022 Summer &quot;北海道・和ハッカ&quot; / Free as a Bird" srcset="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_04b87142-5bd6-4820-8734-8898892447fb.jpg?v=1649157015&amp;width=352 352w" width="520" height="560"></a></div>
 			<div class="txt_wrap height-adjust">
 				<span class="cat_ttl">除菌消臭ミスト</span>
 				<h3 class="prod_name">
-					<a href="/products/k0066"><span class="name en"> Free as a Bird</span><span class="genre added">2022 Summer 北海道・和ハッカ</span></a>
+					<a href="https://komons-japan.com/products/k0066"><span class="name en"> Free as a Bird</span><span class="genre added">2022 Summer 北海道・和ハッカ</span></a>
 				</h3>
 				<span class="product_detail">
 					<span class="price">2,530円(税込)</span>
 				</span>
 			</div>
-			<div class="comp-list-cart-button"><div style="width: 100%; margin: 0 auto; padding: 0">
-				<div class="cartjs_box">
-					<form method="post" action="https://komons-japan.myshopify.com/cart/add" id="product_form_7471770239164" accept-charset="UTF-8" enctype="multipart/form-data" novalidate="novalidate" data-product-form=""><input type="hidden" name="form_type" value="product"><input type="hidden" name="utf8" value="✓">
-						<select name="id" id="ProductSelect-7471770239164" class="product-form__variants no-js" style="display: none;">
-							<option value="41631699927228">Default Title</option>
-						</select>
-						<div class="cartin_button">
-							<button><span>カートに入れる</span></button>
-							<input id="cartSubmit-7471770239164" type="submit" value="カートに入れる">
-						</div>
-					</form>
-				</div>
+			<div class="comp-list-cart-button">
+				<div class="comp-arrow-link">
+        	<a href="https://komons-japan.com/products/k0066"><span>詳細を見る</span></a>
+      	</div>
 			</div>
-		</div>
-	</li>
-	<li class="item_box">
-		<div class="img_wrap"><a href="/products/k0067"><img src="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_702647e1-048a-4bc3-9110-cc8e30dd0f49.jpg?v=1649159168&amp;width=520" alt="2022 Sumer &quot;北海道・和ハッカ&quot; / Free as a Bird(mini)" srcset="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_702647e1-048a-4bc3-9110-cc8e30dd0f49.jpg?v=1649159168&amp;width=352 352w" width="520" height="560"></a></div>
+		</li>
+		<li class="item_box">
+			<div class="img_wrap"><a href="https://komons-japan.com/products/k0067"><img src="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_702647e1-048a-4bc3-9110-cc8e30dd0f49.jpg?v=1649159168&amp;width=520" alt="2022 Sumer &quot;北海道・和ハッカ&quot; / Free as a Bird(mini)" srcset="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_702647e1-048a-4bc3-9110-cc8e30dd0f49.jpg?v=1649159168&amp;width=352 352w" width="520" height="560"></a></div>
 			<div class="txt_wrap height-adjust">
 				<span class="cat_ttl">除菌消臭ミスト</span>
 				<h3 class="prod_name">
-					<a href="/products/k0067"><span class="name en"> Free as a Bird (Mini)</span>
-		<span class="genre added">2022 Summer 北海道・和ハッカ</span></a>
+					<a href="https://komons-japan.com/products/k0067"><span class="name en"> Free as a Bird (Mini)</span>
+						<span class="genre added">2022 Summer 北海道・和ハッカ</span></a>
 				</h3>
 				<span class="product_detail">
 					<span class="price">748円(税込)</span>
 				</span>
 			</div>
-			<div class="comp-list-cart-button"><div style="width: 100%; margin: 0 auto; padding: 0">
-						<div class="cartjs_box">
-						<form method="post" action="https://komons-japan.myshopify.com/cart/add" id="product_form_7471781118140" accept-charset="UTF-8" enctype="multipart/form-data" novalidate="novalidate" data-product-form=""><input type="hidden" name="form_type" value="product"><input type="hidden" name="utf8" value="✓">
-							<select name="id" id="ProductSelect-7471781118140" class="product-form__variants no-js" style="display: none;">
-
-								<option value="41631763562684">
-									Default Title
-								</option>
-
-							</select>
-							<div class="cartin_button">
-								<button><span>カートに入れる</span></button>
-								<input id="cartSubmit-7471781118140" type="submit" value="カートに入れる">
-							</div>
-						</form>
-						</div>
-					</div></div>
+			<div class="comp-list-cart-button">
+				<div class="comp-arrow-link">
+					<a href="https://komons-japan.com/products/k0067"><span>詳細を見る</span></a>
+				</div>
+			</div>
 		</li>
-
 		<li class="item_box">
-			<div class="img_wrap"><a href="/products/k0068"><img src="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_fd0a6455-f2a5-460a-b29c-44d872ec3498.jpg?v=1649159857&amp;width=520" alt="2022 Summer &quot;北海道・和ハッカ&quot; / Free as a Bird(mini・gift)" srcset="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_fd0a6455-f2a5-460a-b29c-44d872ec3498.jpg?v=1649159857&amp;width=352 352w" width="520" height="560"></a></div>
+			<div class="img_wrap"><a href="https://komons-japan.com/products/k0068"><img src="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_fd0a6455-f2a5-460a-b29c-44d872ec3498.jpg?v=1649159857&amp;width=520" alt="2022 Summer &quot;北海道・和ハッカ&quot; / Free as a Bird(mini・gift)" srcset="https://cdn.shopify.com/s/files/1/0608/6233/3116/products/thumb01_fd0a6455-f2a5-460a-b29c-44d872ec3498.jpg?v=1649159857&amp;width=352 352w" width="520" height="560"></a></div>
 			<div class="txt_wrap height-adjust">
 			<span class="cat_ttl">ファブリックミスト</span>
 			<h3 class="prod_name">
-				<a href="/products/k0068"><span class="name en">Free as a Bird (Mini)</span><span class="genre added">北海道・和ハッカ Gift</span></a>
+				<a href="https://komons-japan.com/products/k0068"><span class="name en">Free as a Bird (Mini)</span><span class="genre added">北海道・和ハッカ Gift</span></a>
 			</h3>
 			<span class="product_detail">
 			<span class="price">825円(税込)</span>
 			</span>
 		</div>
-		<div class="comp-list-cart-button"><div style="width: 100%; margin: 0 auto; padding: 0">
-				<div class="cartjs_box">
-					<form method="post" action="https://komons-japan.myshopify.com/cart/add" id="product_form_7471784362172" accept-charset="UTF-8" enctype="multipart/form-data" novalidate="novalidate" data-product-form=""><input type="hidden" name="form_type" value="product"><input type="hidden" name="utf8" value="✓">
-			<select name="id" id="ProductSelect-7471784362172" class="product-form__variants no-js" style="display: none;">
-
-				<option value="41631776440508">
-					Default Title
-				</option>
-
-			</select>
-			<div class="cartin_button">
-				<button><span>カートに入れる</span></button>
-				<input id="cartSubmit-7471784362172" type="submit" value="カートに入れる">
+		<div class="comp-list-cart-button">
+			<div class="comp-arrow-link">
+				<a href="https://komons-japan.com/products/k0068"><span>詳細を見る</span></a>
 			</div>
-		</form>
 		</div>
-	</div></div>
 </li>
 
 
